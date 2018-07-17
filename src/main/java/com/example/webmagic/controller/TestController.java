5ba7993a9e2f880c7c7c1757f943a9e7ccade8de
yitongliu <liuyitong1995@hotmail.com>
2018-07-17 15:11:27 +0800

    update new crawler method

--- src/main/java/com/example/webmagic/controller/TestController.java
+++ src/main/java/com/example/webmagic/controller/TestController.java
@@ -5,6 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -25,5 +32,30 @@ public class TestController {
     @RequestMapping("/spider-baidu")
     public void baiduStart(){
         spiderService.baiduSpider();
+    }
+
+    @RequestMapping("/test-request")
+    public String testRequest(HttpServletRequest request, HttpServletResponse response){
+        Map<String, Object> map = new HashMap<>();
+        map.put("ParameterMap", request.getParameterMap());
+        map.put("Cookies", request.getCookies());
+        map.put("ContextPath", request.getContextPath());
+        map.put("AuthType", request.getAuthType());
+
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements()) {
+            String header = headers.nextElement();
+            String value = request.getHeader(header);
+            map.put(header, value);
+        }
+
+        System.out.println("ha~ now u catch me");
+
+        Cookie cookie1 = new Cookie("cookie1", "111");
+        Cookie cookie2 = new Cookie("cookie2", "222");
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
+
+        return "success";
     }
 }
